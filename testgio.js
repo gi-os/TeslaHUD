7012dd9c4e5c9ae05350a67c187ddfb4017f5257
gi-os <gio@gi-os.com>
2021-01-13 18:15:24 -0500

    f
    f

---
+++ testgio.js
@@ -0,0 +1,140 @@
+var $ = require('jQuery');
+var SpotifyWebApi = require('spotify-web-api-node');
+
+ /**
+  * This example retrieves information about the 'current' user. The current user is the user that has
+  * authorized the application to access its data.
+  */
+
+ /* Retrieve a code as documented here:
+  * https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow
+  *
+  * Codes are given for a set of scopes. For this example, the scopes are user-read-private and user-read-email.
+  * Scopes are documented here:
+  * https://developer.spotify.com/documentation/general/guides/scopes/
+
+
+  $.ajax({
+      type: "GET",
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token: 'AQDS_-KAl3Q_yQ_tBqFpfPNFTQrPH1ba6y4QNrJxAb48FLSReHrllL3WuaZI-7HeFaoPsaYFhpHPD7rzREPQlvadsMwHvZWyIa8IeoGx2IN_sfoE9eW263SZGGY2vKQt3qg'
+      },
+      success: function(data) {
+        console.log(data);
+        console.log(data.item.name);
+        console.log(data.item.artists[0].name);
+
+       var artwork = data.item.album.images[1].url;
+        var trackName = data.item.name;
+        var artistName = data.item.artists[0].name;
+
+        var artworkID = document.getElementById('trackArtwork');
+        var track = document.getElementById('trackName');
+        var artist = document.getElementById('artist');
+
+     artworkID.innerHTML = '<img src=' + artwork + '>';
+       track.textContent = trackName;
+       artist.textContent = 'By ' + artistName;
+
+
+
+      },
+      dataType: "json"
+    });
+http://localhost:8888/#access_token=
+  */
+  var express = require('express'); // Express web server framework
+  var request = require('request'); // "Request" library
+  var cors = require('cors');
+  var querystring = require('querystring');
+  var cookieParser = require('cookie-parser');
+  var app = express();
+  var client_id = '35748c46209741f1a25ea9b20e86098c'; // Your client id
+  var client_secret = '946b5bab9fd048fbb568b32276d60f28'; // Your secret
+  var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+  var access_token = 'BQB5PQ3ht0p84MM1t7rtS0Z8Q6DjXMA5NXVaKX-KGdmzdYn2-gdh1GpBQaLd-bu-ek-pXPQrQLVvwJ6CjjKZIFpgDdRROg8bnWaikLmQBP4dKhFrrUs1-eo_N2RKsdkBTg_kOixkDZrVVDX1aqkqtFbCr3IkSbCbmkFA5g'; // Your redirect uri
+  var access_token2;
+  var request = require('request');
+  type: "GET",
+    request('https://accounts.spotify.com/api/token', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body) // Show the HTML for the Google homepage.
+      }
+})
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  scope: scope,
+  form: {
+
+    redirect_uri: redirect_uri,
+    grant_type: 'authorization_code'
+  },
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  json: true
+};
+var scope = 'user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing';
+request.post(authOptions, function(error, response, body) {
+  //var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')) },
+    scope: scope,
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: 'AQCjeBTTFqVq2dGGjPngB3ha9fXFKHRejdUOOb2iNDijwIcKhuAU1kLIJdec4-RObJK92rac9QdHOE0hgDD3UO4qKizqE9nMw32yGUnijrWEFoU1Uut3wEiNGDcCIAfrxZk'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      access_token2 = body.access_token;
+      access_token = body.access_token;
+      console.log(access_token2)
+    } else{
+      console.log(access_token)
+      //getnowplay()
+    }
+  });
+
+});
+
+
+
+
+
+
+function getnewtoken(){
+  var spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(access_token2);
+  //getnowplay()
+}
+//getnewtoken()
+// set it in the wrapper
+var spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken(access_token);
+function getplaylist(){
+  spotifyApi.getUserPlaylists()
+    .then(function(data) {
+      console.log('User playlists', data['body']['items']);
+    }, function(err) {
+      console.error(err);
+      getnewtoken()
+    });
+}
+function getnowplay(){
+  spotifyApi.getMyCurrentPlayingTrack()
+  .then(function(data) {
+    console.log(data);
+  }, function(err) {
+    console.error(err);
+    getnewtoken()
+  });
+}
+getnowplay()
+getplaylist()
