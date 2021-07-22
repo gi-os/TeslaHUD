be0f884894cf70b6b2e5e029c8677259b9630833
Giovanni Lupo <gio@gi-os.com>
2021-07-22 15:31:27 -0400

    Updated for Control system V2
    included press and scroll
    removed flick for page

--- controllercode/controllercode.ino
+++ controllercode/controllercode.ino
@@ -25,61 +25,122 @@ GND - to microcontroler GND
 
 #define joi 14
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, 4);
-
 int test_limits = 2;
 int allowrot =1;
+int allowpress = 0;
+int allowencoder=0;
 BleKeyboard bleKeyboard;
 void rotary_onButtonClick()
 {
-  static unsigned long lastTimePressed = 0;
-  if (millis() - lastTimePressed < 50)
-    return; //ignore multiple press in that time milliseconds
-  lastTimePressed = millis();
-  //rotaryEncoder.reset();
-  //rotaryEncoder.disable();
-  /*rotaryEncoder.setBoundaries(-test_limits, test_limits, false);
-  test_limits *= 2;*/
-  Serial.print("button pressed at ");
-  Serial.println(millis());
-  bleKeyboard.write('c');
+  if (allowpress == 1){
+    static unsigned long lastTimePressed = 0;
+    if (millis() - lastTimePressed < 50)
+      return; //ignore multiple press in that time milliseconds
+    lastTimePressed = millis();
+    //rotaryEncoder.reset();
+    //rotaryEncoder.disable();
+    /*rotaryEncoder.setBoundaries(-test_limits, test_limits, false);
+    test_limits *= 2;*/
+    Serial.print("button pressed at ");
+    Serial.println(millis());
+    bleKeyboard.write('c');
+    
+  }else{
+    Serial.print("button attempted at ");
+    Serial.println(millis());
+    //allowpress=1;
+    allowencoder=0;
+  }
 }
 int go =0;
+
 void rotary_loop()
 {
   //lets see if anything changed
   int16_t encoderDelta = rotaryEncoder.encoderChanged();
 
-  //optionally we can ignore whenever there is no change
-  if (encoderDelta == 0)
-    //go =0;
-    return;
+  //Serial.println(digitalRead(ROTARY_ENCODER_BUTTON_PIN));
+  if (digitalRead(ROTARY_ENCODER_BUTTON_PIN)==0){
+    //Serial.println(digitalRead(ROTARY_ENCODER_BUTTON_PIN));
+      //optionally we can ignore whenever there is no change
+    allowpress=0;
     
-  //for some cases we only want to know if value is increased or decreased (typically for menu items)
-  if (allowrot ==1){
-      if (encoderDelta > 0){
-      if(encoderDelta > 500){
-        Serial.print("+big");
-        bleKeyboard.write('e');
-      }else{
-        Serial.print("+");
-      bleKeyboard.write('d');
-      //go =1;
-      }
       
-    }else if (encoderDelta < 0){
-      if(encoderDelta < -500){
-        Serial.print("-big");
-        bleKeyboard.write('d');
-      }else{
-        Serial.print("-");
-        bleKeyboard.write('e');
+    if (encoderDelta == 0){
+      if (allowencoder == 0){
+        allowpress=1;
+        //Serial.println("possible");
+        return;
       }
-      
-      //go =-1;
     }
+    
       
+    //for some cases we only want to know if value is increased or decreased (typically for menu items)
+    if (allowrot ==1){
+        if (encoderDelta > 0){
+        if(encoderDelta > 500){
+          allowencoder = 1;
+          Serial.print("+big");
+          bleKeyboard.write('a');
+          
+        }else{
+          allowencoder = 1;
+          Serial.print("+");
+          bleKeyboard.write('b');
+          
+        //go =1;
+        }
+        
+      }else if (encoderDelta < 0){
+        if(encoderDelta < -500){
+          allowencoder = 1;
+          Serial.print("-big");
+          bleKeyboard.write('b');
+          
+        }else{
+          allowencoder = 1;
+          Serial.print("-");
+          bleKeyboard.write('a');
+          
+        }
+        
+        //go =-1;
+      }
+        
+    }
+  }else{
+    //Serial.print("get in to itttt");
+      //optionally we can ignore whenever there is no change
+    if (encoderDelta == 0)
+      //go =0;
+      return;
+      
+    //for some cases we only want to know if value is increased or decreased (typically for menu items)
+    if (allowrot ==1){
+        if (encoderDelta > 0){
+        if(encoderDelta > 500){
+          Serial.print("+big");
+          bleKeyboard.write('e');
+        }else{
+          Serial.print("+");
+        bleKeyboard.write('d');
+        //go =1;
+        }
+        
+      }else if (encoderDelta < 0){
+        if(encoderDelta < -500){
+          Serial.print("-big");
+          bleKeyboard.write('d');
+        }else{
+          Serial.print("-");
+          bleKeyboard.write('e');
+        }
+        
+        //go =-1;
+      }
+        
+    }
   }
-  
 
   //for other cases we want to know what is current value. Additionally often we only want if something changed
   //example: when using rotary encoder to set termostat temperature, or sound volume etc
@@ -150,35 +211,13 @@ void loop() {
     int joival = analogRead(joi);
     //Serial.print("joi");
     //Serial.println((analogRead(joi)));
-  if(joival-center > 200){
-    allowrot=0;
-  //if(joival > 1900){
-    if(joival < 3000){
-      Serial.print("JoiValue: ");
-      Serial.println(joival);
-      bleKeyboard.write('a');
-    }
-  }else if(joival-center < -200){
-    allowrot=0;
-  //} else if (joival<1000){
-    if(joival > 0){
-      Serial.print("JoiValue: ");
-      Serial.println(joival);
-      bleKeyboard.write('b');
-    }
-  }else{
-    if(allowrot==0){
-      allowrot=1;
-      Serial.print("no");
-    }
+  
     
   }
   //in loop call your custom function which will process rotary encoder values
   rotary_loop();
 
   delay(50);
-  if (millis() > 20000)
-    rotaryEncoder.enable();
-  }
+  
 
 }
